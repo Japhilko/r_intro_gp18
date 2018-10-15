157ea8d3bd813c73bedefbf95bcdca6ff4833f87
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2018-10-15 14:17:42 +0200

    data upload

--- rcode/incourse_1.R
+++ rcode/incourse_1.R
@@ -16,3 +16,25 @@ str(b)
 ba <- c("a","b")
 
 ba <- c("a","b",1)
+
+str()
+
+str(b)
+
+b <- c(4,5,6,7,4,3,2,9)
+sample(b,2)
+sample(b,10,replace=T)    
+
+########
+# Lösung Aufgabe A1B
+
+b <- c(1,2,3,4,5)
+b <- 1:5
+b <- seq(1,5)
+
+mean(b)
+var(b)
+sd(b)
+sqrt(mean(b))
+
+##########
